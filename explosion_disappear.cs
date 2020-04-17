b4c2e8fecdfb98fa6ca9aed5d415866a4287041b
algezoly7 <algezoly2020@gmail.com>
2020-04-16 18:55:40 -0700

    new commit

---
+++ explosion_disappear.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class explosion_disappear : MonoBehaviour
+{
+	public float lifeTime = 1f;
+    public GameObject explosion;
+	void Awake()
+    {
+        Destroy(explosion, lifeTime);
+    }
+}
---
+++ look_at.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class look_at : MonoBehaviour
+{
+    public Transform Target;
+    private void Update()
+    {
+        Vector3 direction = -(Target.position - transform.position);
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = rotation;
+    }
+}
